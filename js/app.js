4cebfae13fae03a434c197f41d80692db509d1c2
Trey <Trey@temps-Air-2.attlocal.net>
2020-02-05 20:52:30 -0800

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,31 +1,74 @@
 /*----- constants -----*/
+// Battleship (4) --> ship of size 4
+// Cruiser (3) --> ship of size 3
+// Submarine (3) --> ship of size 3
+// Destroyer (2) --> ship of size 2
+
+/*----- app's state (variables) -----*/
 // Ship Locations 
 // Player 1 ship locations
 //    (Ship 1, ship 2, etc)
 // Cpu ship locations
 //    (Ship 1, ship 2, etc)
 // GuessHit -- Current (guess)
-// numGuesses
+// numGuesses -- Total Guesses
 // Number of total Hits (hits)
 // Is the ship sunk (isSunk = true/false)
+// playerNodes --> board nodes
+// cpuNodes --> board nodes
+// currentTurn --> user/cpu 
 
-/*----- app's state (variables) -----*/
-
-let nodeVal = [];
+let playerNodes = [];
+let cpuNodes = [];
+let currentTurn = 'user'
 
 /*----- cached element references -----*/
-let gameBoard = document.getElementById('game-board');
-/*----- event listeners -----*/
+// gameBoard --> for stored gameboard node ids for player
+// cpuBoard --> for stored gameboard node ids for cpu
+// 
 
-// for (let i = 0; i < cards.length; i++) {
-//   let cardElement = document.createElement('img');
-//   cardElement.setAttribute('src', "images/back.png");
-//   cardElement.setAttribute('data-id', i);
-//   cardElement.addEventListener("click", flipCard);
-//   document.getElementById('game-board').appendChild(cardElement);
-// }
+
+let gameBoard = document.getElementById('game-board');
+let cpuBoard = document.getElementById("cpu-board");
+
+
+/*----- event listeners -----*/
+// selectShip listener
+// placeShip listener
+// playerGuess listener
+
+
+
 
 /*----- functions -----*/
+// createBoard --> Instantiate the board with nodes 10x10 and store info for node IDs
+//  ( a1 --> j10 )
+// appendChildren --> add nodes to the board in HTML
+// addNode --> To add board node within createBoard for HTML updating
+
+// selectShip --> function for allowing user to select a choice of ship size for
+//                placement on the board with subsequent clicks on nodes using placeShip
+// placeShip --> function for placing a ship based on user clicking nodes
+// cpuPlace --> cpu randomly placing ship on board
+//              Can also have this place all ships on the board for CPU at once
+
+
+
+// playerGuess --> get users guess for hit with click on a cpu node 
+// cpuGuess --> randomize cpu guess of a players node
+// checkHit --> check if guess is a hit on a ship
+// applyHit --> applies a hit to the board if checkHit passes
+// applyMiss --> applies a miss to the board if checkHit fails
+// winCheck --> check for win to be called within checkHit 
+// turnSwap --> swap the turn from user to cpu or cpu to user swapping currentTurn
+// toggleBoard --> change board to view the opposite of the currentTurn
+
+
+
+
+
+
+
 function addNode(curNode) {
   let nodeEl = document.createElement('div');
   nodeEl.setAttribute('data-id', curNode);
@@ -39,79 +82,72 @@ function appendChildren(parent, children) {
   children.forEach(function(child){
     parent.appendChild(child)
   })
+  return
 }
+
 
 function createBoard() {
-  let curNode = ''
-  count = 0;
-  // for(let i=1; i<=10; i++) {
-    while(count < 100) {
+  let curNode = 0;
+  for(let i = 1; i <101 ; i++) {
+    curNode = i
+    playerNodes.push(addNode(curNode))
+  }
+  for(let i = -1; i > -101 ; i--) {
+    curNode = i
+    cpuNodes.push(addNode(curNode))
+  }
+    // appendChildren(gameBoard, playerNodes)
+    // appendChildren(cpuBoard, cpuNodes)
+  }
+  
 
-    if(curNode === '' || count < 10 ){
-      curNode = `a${count+1}`
-      count++
-    }
-    else if(10 <= count && count < 20){
-      curNode = `b${count-9}`
-      count++
-    }
-    else if(20 <= count && count < 30){
-      curNode = `c${count-19}`
-      count++
-    }
-    else if(30 <= count && count < 40){
-      curNode = `d${count-29}`
-      count++ 
-    }
-    else if(40 <= count && count < 50){
-      curNode = `e${count-39}`
-      count++
-    }
-    else if(50 <= count && count < 60){
-      curNode = `f${count-49}`
-      count++
-    }
-    else if(60 <= count && count < 70){
-      curNode = `g${count-59}`
-      count++
-    }
-    else if(70 <= count && count < 80){
-      curNode = `h${count-69}`
-      count++
-    }
-    else if(80 <= count && count < 90){
-      curNode = `i${count-79}`
-      count++
-    }
-    else if(90 <= count){
-      curNode = `j${count-89}`
-      count++
-    }
-    nodeVal.push(addNode(curNode));
 
-    // if(count % 10 === 0) {
-    //   i = 0;
-    // }
+  function render(cb) {
+    cb()
   }
 
-  appendChildren(gameBoard, nodeVal)
-}
+  function playerSwap() {
+    if(currentTurn === 'user'){
+      cpuBoard.style.opacity = '0%'
+      gameBoard.style.opacity = '100%'
+      appendChildren(gameBoard, playerNodes)
+      currentTurn = 'cpu'
+
+    }
+
+    else if(currentTurn === 'cpu') {
+      gameBoard.style.opacity = '0%'
+      cpuBoard.style.opacity = '100%'
+      appendChildren(cpuBoard, cpuNodes)
+      currentTurn = 'user'
+    }
+  }
 
 /*----- GAME START -----*/
+
+// Render Board
+// Cpu places ships
+// Player selects ship to place
+// Player places ship with click on node then click on adjacent node in direction NSEW
+// Alternatively... selects each node along a path for ship if auto place is too hard
+
+// Begin game with player turn
 
 
 // Are all ships sunk? (While loop?) --Player / Cpu
 //  Player Guess
-//  Is guess valid? -- if no tell user they need to enter a proper board spot
-//  If yes -- +1 guesses
-//  Did it hit?
-//  Yes ---- No
-//  Check against CPU ship 1 - Ship X locations
-//  If hit check length of ship and add one to hits
-//  If hits === ship length
-//  isSunk === True
-//   SHIP IS SUNK!
-//  If no hit CPU Guess
-//  CPU Guessing AI -- If hit check nsew nodes -- if 2 hit check in line nodes
-//  SAME CHECK AS ABOVE
+//  Check Hit --> Place Hit && Check Sunk && Check Win || Place Miss --> Swap Turn
+//  CPU Guess
+//  Check Hit --> Place Hit && Check Sunk && Check Win || Place Miss -->  Swap Turn
+
+
+
+
 createBoard();
+
+// playerSwap(appendChildren(gameBoard, playerNodes))
+// playerSwap();
+// console.log('hello2')
+
+
+
